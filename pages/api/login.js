9e4abc3ed5ad76183636b475f069c23a3c64278a
Maricris Bonzo <maricris@magic.link>
2021-01-08 13:39:00 -0800

    Fix error when calling Go server

--- pages/api/login.js
+++ pages/api/login.js
@@ -15,33 +15,6 @@ export default async function login(req, res) {
     */
     await setLoginSession(res, loggedInUserInfo);
 
-    /* 
-    Below is a fetch request to the local Go server.
-    This is the first step in saving the authenticated user's info.
-    Second step happens at the server side; where you call your 
-    application logic to save the user's info in some kind of database.
-    */
-    const resFromGoServer = await fetch("https://scrappy-twitter-api-client.vercel.app/login", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-        Authorization: "Bearer " + accessToken,
-      },
-      body: JSON.stringify(req.body)
-    });
-
-    if (resFromGoServer.status === 200) {
-      console.log(
-        "YAY, we were able to call the login endpoint: ",
-        resFromGoServer
-      );
-    } else {
-      throw new Error(
-        `Oh no, we ran into an error while calling the login endpoint: `,
-        await resFromGoServer.text()
-      );
-    }
-
     res.status(200).send({ done: true });
   } catch (error) {
     res.status(error.status || 500).end(error.message);
--- pages/login.js
+++ pages/login.js
@@ -47,17 +47,53 @@ const Login = () => {
         body: JSON.stringify(body),
       });
       if (res.status === 200) {
-        /*
+        /* 
+    Below is a fetch request to the local Go server.
+    This is the first step in saving the authenticated user's info.
+    Second step happens at the server side; where you call your 
+    application logic to save the user's info in some kind of database.
+    */
+        const resFromGoServer = await fetch(
+          "https://scrappy-twitter-api-client.vercel.app/login",
+          {
+            method: "POST",
+            headers: {
+              "Content-Type": "application/json",
+              Authorization: "Bearer " + accessToken,
+            },
+            body: JSON.stringify(body),
+          }
+        );
+
+        console.log('resFromGoServer: ', resFromGoServer)
+        console.log('resFromGoServer.status: ', resFromGoServer.status)
+
+        if (resFromGoServer.status === 200) {
+          console.log(
+            "YAY, we were able to call the login endpoint: ",
+            resFromGoServer
+          );
+
+                  /*
         User was able to log in or sign up successfully,
         so we move them to the home page along with their
         accessToken.
         */
         Router.push("/");
+
+        } else {
+          throw new Error(
+            `Oh no, we ran into an error while calling the login endpoint: `,
+            await resFromGoServer.text()
+          );
+        }
+
+
       } else {
         throw new Error(await res.text());
       }
     } catch (error) {
-      console.error("An unexpected error happened occurred:", error);
+      console.error("An unexpected error occurred:", error);
       setErrorMsg(error.message);
     }
   }
