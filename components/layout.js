54ed4badd3b651c4ea02fed37f3dc10bf003c040
Maricris Bonzo <maricris@magic.link>
2021-01-07 18:28:18 -0800

    Remove Vercel

--- components/layout.js
+++ components/layout.js
@@ -14,16 +14,6 @@ const Layout = (props) => (
       <div className="container">{props.children}</div>
     </main>
 
-    <footer>
-      <a
-        href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        Powered by <img src="/vercel.svg" alt="Vercel Logo" />
-      </a>
-    </footer>
-
     <style jsx global>{`
       *,
       *::before,
